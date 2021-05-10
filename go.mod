52da4cd8d40aedb04cec0a06fccc487133c3e505
Yiğit Sadıç <yigitsadic@gmail.com>
2021-05-10 17:47:41 +0300

    refactor

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/yigitsadic/hsedocumentgenerator
 go 1.16
 
 require (
-	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
-	github.com/thecodingmachine/gotenberg-go-client/v7 v7.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/thecodingmachine/gotenberg-go-client/v7 v7.1.0
 )
--- internal/pdf_generator/pdf_generator.go
+++ internal/pdf_generator/pdf_generator.go
@@ -1,7 +1,71 @@
 package pdf_generator
 
-import "github.com/thecodingmachine/gotenberg-go-client/v7"
+import (
+	"bytes"
+	"fmt"
+	"github.com/thecodingmachine/gotenberg-go-client/v7"
+	"github.com/yigitsadic/hsedocumentgenerator/internal/models"
+)
 
 type PDFGenerate interface {
 	Build(req *gotenberg.HTMLRequest) ([]byte, error)
+	BuildRequest(r models.Record, store *AssetStore) (*gotenberg.HTMLRequest, error)
+}
+
+type PDFGenerator struct {
+	Store *AssetStore
+}
+
+// Builds request for gotenberg with given record.
+func (g *PDFGenerator) BuildRequest(r models.Record) (*gotenberg.HTMLRequest, error) {
+	htmlContent := new(bytes.Buffer)
+	g.Store.Template.Execute(htmlContent, r)
+
+	qr, err := r.GenerateQRCode()
+	if err != nil {
+		return nil, err
+	}
+
+	index, err := gotenberg.NewDocumentFromString(fmt.Sprintf("%s.pdf", r.UniqueReference), htmlContent.String())
+	if err != nil {
+		return nil, err
+	}
+
+	img1, err := gotenberg.NewDocumentFromBytes("1920px-Emblem_of_Uzbekistan.svg.png", g.Store.StateLogo)
+	if err != nil {
+		return nil, err
+	}
+
+	img2, err := gotenberg.NewDocumentFromBytes("bg.jpg", g.Store.Background)
+	if err != nil {
+		return nil, err
+	}
+
+	img3, err := gotenberg.NewDocumentFromBytes("hse_logo.png", g.Store.HseLogo)
+	if err != nil {
+		return nil, err
+	}
+
+	img4, err := gotenberg.NewDocumentFromBytes("kase.png", g.Store.CompanySignature)
+	if err != nil {
+		return nil, err
+	}
+
+	img5, err := gotenberg.NewDocumentFromBytes("qr_code.png", qr)
+	if err != nil {
+		return nil, err
+	}
+
+	css, err := gotenberg.NewDocumentFromBytes("styles.css", g.Store.Styles)
+	if err != nil {
+		return nil, err
+	}
+
+	req := gotenberg.NewHTMLRequest(index)
+	req.Landscape(true)
+	req.PaperSize(gotenberg.A4)
+	req.Assets(img1, img2, img3, img4, img5, css)
+	req.Margins(gotenberg.NoMargins)
+
+	return req, nil
 }
