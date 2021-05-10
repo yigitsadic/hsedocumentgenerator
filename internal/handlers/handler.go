3c367691771887a3cd7542f51d850954223c7042
Yiğit Sadıç <yigitsadic@gmail.com>
2021-05-10 11:41:49 +0300

    okunan kayıtlar için okundu mesajı

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -2,26 +2,46 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/yigitsadic/hsedocumentgenerator/internal/models"
 	"github.com/yigitsadic/hsedocumentgenerator/internal/sheet_reader"
 	"io"
 )
 
 const (
-	welcomeText              = "🚀 Google Sheets üzerinden okuma başlatıldı."
-	recordReadText           = "📗 Google Sheets üzerinden %d kayıt okundu."
+	welcomeText              = "🚀 Google Sheets üzerinden okuma başlatıldı.\n"
+	recordReadText           = "📗 Google Sheets üzerinden %d kayıt okundu.\n"
+	cannotReadFromGoogleText = "😥 Google Sheets üzerinden kayıtlar okunamadı.\n"
 	outputZIPText            = "🤔 Oluşturulan PDFleri içeren ZIP dosyası nereye depolanacak?:"
-	pdfGenerationStartedText = "⏳ PDF belge üretme işlemi başlandı..."
-	pdfGeneratedText         = "👍 [%d/%d] PDF belgesi üretildi."
-	zipFileCreatedText       = "✅ PDF belgeleri %q olarak sıkıştırıldı ve Google Sheets içine eklendi."
-	processSucceededText     = "💫 İşlem tamamlandı. İyi günler!"
+	pdfGenerationStartedText = "⏳ PDF belge üretme işlemi başlandı...\n"
+	pdfGeneratedText         = "👍 [%d/%d] PDF belgesi üretildi.\n"
+	zipFileCreatedText       = "✅ PDF belgeleri %q olarak sıkıştırıldı ve Google Sheets içine eklendi.\n"
+	processSucceededText     = "💫 İşlem tamamlandı. İyi günler!\n"
 )
 
 type Handler struct {
-	Output io.Writer
-	Client *sheet_reader.SheetClient
+	Output      io.Writer
+	Client      sheet_reader.SheetClient
+	ReadRecords []models.Record
+
+	ZipOutputPath string
 }
 
 // Greets user.
 func (h *Handler) PrintHelloText() {
-	fmt.Fprintln(h.Output, welcomeText)
+	fmt.Fprint(h.Output, welcomeText)
+}
+
+// Reads from Google Sheet client and prints output
+func (h *Handler) ReadFromSheets() error {
+	result, err := h.Client.ReadFromSheets()
+	if err != nil {
+		fmt.Fprint(h.Output, cannotReadFromGoogleText)
+
+		return err
+	}
+
+	h.ReadRecords = result
+
+	fmt.Fprintf(h.Output, fmt.Sprintf(recordReadText, len(h.ReadRecords)))
+	return nil
 }
--- internal/handlers/handler_test.go
+++ internal/handlers/handler_test.go
@@ -2,9 +2,22 @@ package handlers
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
+	"github.com/yigitsadic/hsedocumentgenerator/internal/models"
+	"reflect"
 	"strings"
 	"testing"
 )
+
+type mockClient struct {
+	Output []models.Record
+	Error  error
+}
+
+func (m mockClient) ReadFromSheets() ([]models.Record, error) {
+	return m.Output, m.Error
+}
 
 func TestHandler_PrintHelloText(t *testing.T) {
 	o := new(bytes.Buffer)
@@ -15,4 +28,54 @@ func TestHandler_PrintHelloText(t *testing.T) {
 	if !strings.Contains(o.String(), welcomeText) {
 		t.Errorf("expected to see %q but got %q", welcomeText, o.String())
 	}
+}
+
+func TestHandler_ReadFromSheets(t *testing.T) {
+	t.Run("it should handle successful scenario", func(t *testing.T) {
+		mC := new(mockClient)
+		o := new(bytes.Buffer)
+		h := Handler{Output: o, Client: mC}
+
+		mC.Output = []models.Record{
+			{
+				FirstName:       "Ali",
+				LastName:        "Veli",
+				JobName:         "Vinç Operatörü",
+				CompanyName:     "Bir Şirket",
+				EducationDate:   "07.02.2012",
+				EducationName:   "Güvenli Sürüş",
+				EducationHours:  "22 saat",
+				UniqueReference: "ABCDEF",
+			},
+		}
+		mC.Error = nil
+
+		h.ReadFromSheets()
+
+		if !strings.Contains(o.String(), fmt.Sprintf(recordReadText, len(mC.Output))) {
+			t.Errorf("expected output not satisfied")
+		}
+
+		if !reflect.DeepEqual(mC.Output, h.ReadRecords) {
+			t.Errorf("expected to read records assign")
+		}
+	})
+
+	t.Run("it should handle failure scenario", func(t *testing.T) {
+		mC := new(mockClient)
+		o := new(bytes.Buffer)
+		h := Handler{Output: o, Client: mC}
+
+		mC.Output = nil
+		mC.Error = errors.New("some error occurred")
+
+		err := h.ReadFromSheets()
+		if err == nil {
+			t.Errorf("expected to see error but got nothing")
+		}
+
+		if !strings.Contains(o.String(), cannotReadFromGoogleText) {
+			t.Errorf("expected output not satisfied. expected=%q but got=%q", cannotReadFromGoogleText, o.String())
+		}
+	})
 }
