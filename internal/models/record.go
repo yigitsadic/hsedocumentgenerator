f96d6bc9b6a5c077443bcbd797e7a491788a386d
Yiğit Sadıç <yigitsadic@gmail.com>
2021-05-10 17:34:27 +0300

    generate qr code testi eklendi.

--- internal/models/record.go
+++ internal/models/record.go
@@ -1,13 +1,21 @@
 package models
 
 import (
+	"errors"
 	gonanoid "github.com/matoous/go-nanoid/v2"
+	"github.com/skip2/go-qrcode"
 	"log"
 )
 
 const (
 	charset = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-"
 	length  = 17
+
+	baseURL = "https://hsegroup.uz/kurumsal/certificate_verification?qr_code="
+)
+
+var (
+	UniqueReferenceMustPresentErr = errors.New("unique reference must present")
 )
 
 type Record struct {
@@ -21,6 +29,7 @@ type Record struct {
 	UniqueReference string
 }
 
+// Assigns unique nano id reference code to object.
 func (r *Record) AssignUniqueReference() {
 	code, err := gonanoid.Generate(charset, length)
 	if err != nil {
@@ -30,4 +39,13 @@ func (r *Record) AssignUniqueReference() {
 	}
 
 	r.UniqueReference = code
+}
+
+// Creates QR code for record and returns it as bytes.
+func (r Record) GenerateQRCode() ([]byte, error) {
+	if r.UniqueReference == "" {
+		return nil, UniqueReferenceMustPresentErr
+	}
+
+	return qrcode.Encode(baseURL+r.UniqueReference, qrcode.Medium, 100)
 }
--- internal/models/record_test.go
+++ internal/models/record_test.go
@@ -1,6 +1,8 @@
 package models
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestRecord_AssignUniqueReference(t *testing.T) {
 	r := Record{}
@@ -14,4 +16,26 @@ func TestRecord_AssignUniqueReference(t *testing.T) {
 	if len(r.UniqueReference) != length {
 		t.Errorf("unexpected to see %d char unique reference expected was=%d", len(r.UniqueReference), length)
 	}
+}
+
+func TestRecord_GenerateQRCode(t *testing.T) {
+	t.Run("it should give error if reference code is blank", func(t *testing.T) {
+		r := Record{UniqueReference: ""}
+
+		_, err := r.GenerateQRCode()
+
+		if err != UniqueReferenceMustPresentErr {
+			t.Errorf("expected to get %s error but got %s", UniqueReferenceMustPresentErr, err)
+		}
+	})
+
+	t.Run("it should generate qr code", func(t *testing.T) {
+		r := Record{UniqueReference: "ABCDEF"}
+
+		_, err := r.GenerateQRCode()
+
+		if err != nil {
+			t.Errorf("unexpected to get an error but got=%s", err)
+		}
+	})
 }
